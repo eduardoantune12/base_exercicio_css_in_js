76893a36cdb3ad8924ea9fa1503bd26569cf9779
Eduardo Antunes <eduardoantune12@gmail.com>
2025-06-10 15:48:37 -0300

    Adicionando arquivos transformados em Styled Components

---
+++ Cabecalho.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  background-color: #eee;
+  padding: 16px;
+  color: #333;
+`
+
+export const Titulo = styled.h1`
+  font-size: 24px;
+  margin: 0;
+`
